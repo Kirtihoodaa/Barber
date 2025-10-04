af35ab638ac9a660bcbb8fdb4d8036955140329f
Kirti Hooda <kirtihooda0033@gmail.com>
2025-10-04 23:43:00 +0530

    Folder Initlaization

---
+++ lib/CustomerScreens/CustomerHomePage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Customerhomepage extends StatelessWidget {
+  const Customerhomepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Customer Home Page")));
+  }
+}
